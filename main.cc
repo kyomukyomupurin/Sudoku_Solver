c790ce15cd677ce69da9e4f028fdc830d142cb0c
kyomukyomupurin <murakami1728@gmail.com>
2020-10-16 10:42:31 +0900

    :sparkles: Update

--- main.cc
+++ main.cc
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <vector>
 
-class SudokuSolver {
+class WonderfulSudokuSolver {
   static constexpr size_t N = 9;
   using Board = std::array<std::array<int, N>, N>;
   std::pair<int, int> NotFound = {-1, -1};
   Board board;
 
  public:
-  SudokuSolver() = default;
+  WonderfulSudokuSolver() = default;
 
   void Input() {
     for (int i = 0; i < N; ++i) {
@@ -26,7 +26,7 @@ class SudokuSolver {
     if (FindEmpty() == NotFound) {
       return;
     }
-    auto [empty_column, empty_row] = FindEmpty();
+    auto [empty_row, empty_column] = FindEmpty();
   }
 
   void Output() {
@@ -41,39 +41,47 @@ class SudokuSolver {
  private:
   bool Placeable_Row(int id, int number) {
     for (int i = 0; i < N; ++i) {
-      if (board[id][i] == number) {
-        return false;
-      }
+      if (board[id][i] == number) return false;
     }
     return true;
   }
 
   bool Placeable_Column(int id, int number) {
     for (int i = 0; i < N; ++i) {
-      if (board[i][id] == number) {
-        return false;
-      }
+      if (board[i][id] == number) return false;
     }
     return true;
   }
 
-  bool Placeable_Block(int id, int number) {}
+  bool Placeable_Block(int id_row, int id_column, int number) {
+    // TO DO
+  }
 
   std::pair<int, int> FindEmpty() {
     for (int i = 0; i < N; ++i) {
       for (int j = 0; j < N; ++j) {
-        if (board[i][j] == 0) {
-          return {i, j};
-        }
+        if (board[i][j] == 0) return {i, j};
       }
     }
     return {-1, -1};
   }
+
+  std::vector<int> GetPlaceableNumbers(int empty_row, int empty_column) {
+    std::vector<int> res;
+    for (int number = 1; number <= N; ++number) {
+      bool can = true;
+      if (!Placeable_Row(empty_row, number)) can = false;
+      if (!Placeable_Column(empty_column, number)) can = false;
+      if (!Placeable_Block(empty_row, empty_column, number)) can = false;
+      if (can) res.emplace_back(number);
+    }
+    return res;
+  }
 };
 
 int main() {
-  SudokuSolver solver;
+  WonderfulSudokuSolver solver;
   solver.Input();
-  solver.Solve();
+  // solver.Solve();
   solver.Output();
 }
